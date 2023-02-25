c10113e9eacd56f2b6cad5baf740bd635d7abf10
Kristie Patterson <kpatterson395@gmail.com>
2023-02-25 09:40:07 -0500

    whoops

--- battleship/src/Opponent.js
+++ battleship/src/Opponent.js
@@ -28,7 +28,7 @@ const Opponent = ({ board, addSelected, handleError }) => {
                                         return (
                                             <GamePiece
                                                 handleClick={(e) => handleSelect(`${row}${i}`, e)}
-                                                opponent={false}
+                                                opponent={true}
                                                 hit={board.hits.includes(`${row}${i}`)}
                                                 ship={isShip(`${row}${i}`, board.battleships)}
                                                 key={uuidv4()}
