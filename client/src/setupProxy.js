26d6230246f1411e7726673033eca55f461535b8
Kristie Patterson <kristiepatterson@Kristies-MacBook-Pro.local>
2023-03-22 18:46:34 -0400

    try deploy

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,10 +1,13 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
+const PORT = process.env.PORT || 3001;
+
 module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:3001',
+      
+      target: `http://localhost:${PORT}`,
       changeOrigin: true,
     })
   );
